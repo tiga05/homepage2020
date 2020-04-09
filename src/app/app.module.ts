34f6ad884783e001c0d708b8517c5051c3eb3d63
tiga05 <tiga05@gmx.de>
2020-04-09 15:19:44 +0200

    added pages and first header test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    Page4Component,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
